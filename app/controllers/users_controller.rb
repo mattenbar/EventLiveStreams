820614ae788a2f99ce51b9fc812b4455bdcb71ff
mattenbar <matthw.enbar@gmail.com>
2020-07-06 17:06:59 -0400

    users controller created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,23 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      session[:user] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def show
+    @user = User.find_by(:id => params[:id])
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :user_name, :email, :password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   has_many :favorites 
   has_many :favorited_events, through: :favorites, source: :event
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :user_name, uniqueness: true
+  validates :email, uniqueness: true
+
   has_secure_password
 end
